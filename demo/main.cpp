f289ab771e92dd731b5d3b211c5d847fd3cbc118
Cowsheen <kocjan.krzysztof15@gmail.com>
2026-08-30 11:00:25 +0200

    multi-threaded demo
    Task-Id: 260830ujn

--- demo/main.cpp
+++ demo/main.cpp
@@ -2,16 +2,29 @@
 #include "macros.hpp"
 
 int main() {
+    auto& log = cnx::CinVox::instance();
 
-    auto& logger = cnx::CinVox::instance();
-    logger.set_file_output("log.txt");
-    logger.set_console_output(true);
+    log.set_file_output("log.txt");
+    log.set_min_log_level(cnx::LogLevel::Debug);
+    log.set_console_output(true);
 
-    logger.trace("direct call without location tracking");
+    {
+        std::vector<std::jthread> pool;
+        for (int t = 0; t < 8; ++t) {
+            pool.emplace_back([t] {
+                for (int i = 0; i < 100; ++i) {
+                    INFO("thread {} iteration {}, value = {:.2f}", t, i, i * 1.5);
+                    WARNING("thread {} slow iteration {}", t, i);
+                    DEBUG("thread {} debug payload {}", t, i * i);
+                }
+            });
+        }
+    } 
+    
+    log.trace("direct call without location tracking");
 
     auto location = std::source_location::current();
-    logger.trace(location, "explicit location: {}:{}", location.file_name(), location.line());
-
+    log.trace(location, "explicit location: {}:{}", location.file_name(), location.line());
 
     TRACE("Trace from macro")
     DEBUG("Debug from macro")
